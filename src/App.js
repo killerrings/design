e3e5a500dc7522b2202c67ff63996163797485f5
killerrings <swarjgogoi68@gmail.com>
2023-09-13 18:07:51 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Hero from "./components/Hero";
+
+function App() {
+  return (
+    <div className="App flex items-center justify-center h-screen">
+      <Hero />
+    </div>
+  );
+}
+
+export default App;
